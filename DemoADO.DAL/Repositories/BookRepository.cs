1c8105ae1f9da287a4f0bb27caf4354fa46f5bdf
technifutur <technifutur@DESKTOP-EHN422D>
2023-04-28 10:08:13 +0200

    Ajoutez des fichiers projet.

---
+++ DemoADO.DAL/Repositories/BookRepository.cs
@@ -0,0 +1,40 @@
+﻿using DemoADO.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoADO.DAL.Repositories
+{
+    public class BookRepository
+    {
+        private readonly string _connectionString = "Server=DESKTOP-EHN422D;Database=DemoADO;Trusted_Connection=true;";
+        private readonly IDbConnection _connection;
+
+        public BookRepository()
+        {
+            _connection = new SqlConnection(_connectionString);
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            using (IDbCommand command = _connection.CreateCommand())
+            {
+                command.CommandText = "SELECT * FROM BOOK";
+
+                _connection.Open();
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DemoADO.Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoADO.Models
+{
+    public class Book
+    {
+        public string Isbn { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public string? Description { get; set; }
+        public string Category { get; set; } = null!;
+    }
+}
